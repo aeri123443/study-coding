700b36386b265d0cc2e1eacfbcd129f13a47f829
애리 <aeri123443@gmail.com>
2025-02-28 12:05:59 +0900

    [코테100제] 11

---
+++ code100/11-try1.js
@@ -0,0 +1,26 @@
+/**
+ * p.161 10. 짝지어 제거하기
+ * 소요시간: 16m 28s
+ */
+
+function solution(s){
+    if (s.length%2 === 1) {return 0}
+
+    let stack = [];
+    for (c of s){
+        if (stack.length===0) {stack.push(c); continue;}
+
+        // pop과 같은건지 확인
+        const last = stack.pop();
+        if (last !== c) { stack.push(last, c);}
+        // console.log(last, c, stack);
+    }
+    // 스택 비어있으면 1, 아니면 0 반환
+    return stack.length===0 ? 1 : 0;
+}
+
+console.log(solution("baabaa")); // 1
+console.log(solution("cdcd")); // 0
+console.log(solution("abbcca")); // 1
+console.log(solution("aaa")); //0
+console.log(solution("abbcac")); // 0
---
+++ code100/11-try2.js
@@ -0,0 +1,25 @@
+/**
+ * p.161 10. 짝지어 제거하기
+ * pop 대신 length 활용해보기
+ */
+
+function solution(s){
+    if (s.length%2 === 1) {return 0}
+
+    let stack = [];
+    for (c of s){
+        // pop과 같은건지 확인
+        const len = stack.length;
+        if ( (len>0) && (stack[len-1]===c) ) {stack.pop()}
+        else {stack.push(c)}
+        // console.log(last, c, stack);
+    }
+    // 스택 비어있으면 1, 아니면 0 반환
+    return stack.length===0 ? 1 : 0;
+}
+
+console.log(solution("baabaa")); // 1
+console.log(solution("cdcd")); // 0
+console.log(solution("abbcca")); // 1
+console.log(solution("aaa")); //0
+console.log(solution("abbcac")); // 0
