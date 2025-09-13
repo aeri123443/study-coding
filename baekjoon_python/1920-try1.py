fcd8c8c5e81609231f6cb5a195938602c0a37ab5
애리 <aeri123443@gmail.com>
2025-09-13 10:30:54 +0900

    [백준] Python 1920

---
+++ baekjoon_python/1920-try1.py
@@ -0,0 +1,15 @@
+'''
+1920. 수 찾기
+https://www.acmicpc.net/problem/1920
+'''
+
+N = int(input())
+A = set(map(int, input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+
+for b in B:
+    if b in A:
+        print(1)
+    else: print(0)
+              
