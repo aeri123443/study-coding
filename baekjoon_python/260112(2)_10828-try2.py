189385195ba67255d8a6197a532f9a8163c08f8b
애리 <aeri123443@gmail.com>
2026-01-12 10:45:26 +0900

    [백준] Python 10828

---
+++ baekjoon_python/260112(2)_10828-try2.py
@@ -0,0 +1,30 @@
+'''
+10828. <실버4> 스택
+https://www.acmicpc.net/problem/10828
+짧게 치기
+'''
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for _ in range(N):
+
+    cmd = input().split()
+
+    if cmd[0] == 'push':
+        stack.append(cmd[1])
+
+    elif cmd[0] == 'pop':
+        print(stack.pop() if stack else -1)
+
+    elif cmd[0] == 'size':
+        print(len(stack))
+
+    elif cmd[0] == 'empty':
+        print(0 if stack else 1)
+        
+    elif cmd[0] == 'top':
+        print(stack[-1] if stack else -1)
